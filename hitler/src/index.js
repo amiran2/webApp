4f97a374596619b83cc9822f4ba24e147fb0d2e5
amiran2 <steamda52@gmail.com>
2025-07-14 19:33:10 +0300

    first commit

---
+++ hitler/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {render} from "@testing-library/react";
+
+function App() {
+    return (
+        <div className="">
+            <span>Hello</span>
+        </div>
+    )
+}
+
